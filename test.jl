bdb8676f6eb3188bc87e257108e634245f84e86a
Johan Larsson <mail@larssonjohan.com>
2020-07-02 14:47:30 +0200

    simplify example

--- test.jl
+++ test.jl
@@ -3,15 +3,8 @@ using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 
-using Plots
 using CSV
-#using GR
 using DataFrames
-
-#gr()
-
-#plot(1:10, 1:10)
-#savefig("test.pdf")
 
 a = DataFrame(a = 1:3)
 CSV.write("a.csv", a)
